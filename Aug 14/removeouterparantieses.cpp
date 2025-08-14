be5404ae4cafe64c1928e9b8e3ba361c659338d0
thanushachinnari <thanushachinnari2003@gmail.com>
2025-08-14 14:59:12 +0530

    removing outer parenthesis

---
+++ Aug 14/removeouterparantieses.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+string removeOuter(string s){
+      int n = s.size();
+      int count = 0;
+      string result = "";
+      for(int i =0;i<n;i++){
+        
+         if(s[i]==')'){
+          count--;
+         }
+         if(count!=0){
+          result = result+s[i];
+         }
+          if(s[i]=='(')
+         {
+          count++;
+         }
+      }
+
+  return result;
+}
+int main(){
+  string s = "(()()))(())";
+   string op = removeOuter(s);
+   cout<<"after removing outmost parenthesis is:"<<op;
+  return 0;
+
+}
---
+++ hashing.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i =0;i<n;i++){
+    cin>>arr[i];
+  }
+  int q;
+  cin>>q;
+  int hash[13] = {0};
+  for(int i =0;i<n;i++){
+        hash[arr[i]]+=1;
+  }
+  while(q--){
+    int number;
+    cin>>number;
+    cout<<hash[number] <<endl;
+  }
+  return 0;
+}
